dccf9e9663f0d8bc29f6751bf85f8cff228304f8
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-20 21:52:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,33 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+  end
+  
+  def initialize
+  end 
+  
+  def table_name_for_insert
+  end 
+  
+  def col_names_for_insert
+  end 
+  
+  def values_for_insert
+end
+
+  def save
+  end 
+  
+  def self.find_by_name
+  end 
+  
+  def self.find_by
+  end 
+  
   
 end
