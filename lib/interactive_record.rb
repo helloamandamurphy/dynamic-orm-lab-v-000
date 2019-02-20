e65e07b6c97d0be05c848a8247c3737d5fae8910
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-20 22:22:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -46,10 +46,15 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end 
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end 
   
-  def self.find_by
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first 
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = "
   end 
   
   
